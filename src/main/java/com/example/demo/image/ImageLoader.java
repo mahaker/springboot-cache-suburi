b2c475daf0595977e530151ab27d6bb265887486
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-07-07 19:12:00 +0900

    enable cache

--- src/main/java/com/example/demo/image/ImageLoader.java
+++ src/main/java/com/example/demo/image/ImageLoader.java
@@ -1,12 +1,11 @@
 package com.example.demo.image;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +13,7 @@ public class ImageLoader {
 
     private final Logger logger = Logger.getLogger("ImageLoader");
 
+    @Cacheable("image")
     public byte[] load(String path, String id) {
         var imagePath = path + "/" + "img" + id + ".png";
         logger.log(Level.INFO, "load {0}", imagePath);
