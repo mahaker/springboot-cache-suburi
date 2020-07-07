bd80b41356b2abd74e63c768ebc628e7b03e0086
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-07-07 18:29:56 +0900

    get path valiable and easy logging

--- src/main/java/com/example/demo/web/ImageResource.java
+++ src/main/java/com/example/demo/web/ImageResource.java
@@ -1,14 +1,23 @@
 package com.example.demo.web;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageResource {
 
-    @GetMapping("/image")
-    public ResponseEntity<String> getImage() {
+    final Logger logger = Logger.getLogger("ImageResourceLogger");
+
+    @GetMapping("/image/{imageId}")
+    public ResponseEntity<String> getImage(
+        @PathVariable String imageId
+    ) {
+        logger.log(Level.INFO, "imageId={0}", imageId);
         return ResponseEntity.ok("Hello world.");
     }
 }
